ae1a9597f9d40b68e64c29b4b6eec556feeb4cf3
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2025-02-15 22:27:22 +0330

    In this commit, I created the repository and service files for the models, except for pageComments, which I will complete in the next commits.

---
+++ DataLayer/Services/PageGroupRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class PageGroupRepository : IPageGroupRepository
+    {
+        MyCmsContext db = new MyCmsContext();
+        public IEnumerable<PageGroup> GetAllGroups()
+        {
+            return db.PageGroups;
+        }
+
+        public PageGroup GetGroupById(int groupId)
+        {
+            return db.PageGroups.Find(groupId); 
+        }
+
+        public bool InsertGroup(PageGroup pageGroup)
+        {
+            try
+            {
+                db.PageGroups.Add(pageGroup);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateGroup(PageGroup pageGroup)
+        {
+            try
+            {
+                db.Entry(pageGroup).State = EntityState.Modified;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteGroup(PageGroup pageGroup)
+        {
+            try
+            {
+                db.Entry(pageGroup).State = EntityState.Deleted;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        }
+
+        public bool DeleteGroup(int groupId)
+        {
+            try
+            {
+                var group = GetGroupById(groupId);
+                DeleteGroup(group);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
