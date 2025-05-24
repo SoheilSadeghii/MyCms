f454e6ecdc3ab401e7e5c05927407b13d5636833
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2025-05-24 20:36:27 +0330

    Added LoginViewModel with fields for username, password, and remember options.

---
+++ DataLayer/ViewModels/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "UserName")]
+        [Required(ErrorMessage = "Please enter {0}")]
+        [MaxLength(200)]
+        public string UserName { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Please enter {0}")]
+        [MaxLength(200)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }    
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
