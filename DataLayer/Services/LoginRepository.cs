db05e454a6c1deb3fa57ed135ab49fbe79d196b4
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2025-05-24 20:18:56 +0330

    Added AdminLogin to the repository.

---
+++ DataLayer/Services/LoginRepository.cs
@@ -0,0 +1,23 @@
+﻿using DataLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class LoginRepository : ILoginRepository
+    {
+        private MyCmsContext db;
+        public LoginRepository(MyCmsContext context)
+        {
+            db = context;
+        }
+        public bool IsExitUser(string username, string password)
+        {
+            return db.AdminLogins.Any(u => u.UserName == username && u.Password == password);
+        }
+    }
+}
