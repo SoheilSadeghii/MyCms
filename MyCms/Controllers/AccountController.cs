0f057ebbb3f5fe6475a726d18c03dcb8e3cd8b81
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2025-05-24 20:28:40 +0330

    Added AccountController for AdminLogin.

---
+++ MyCms/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using DataLayer;
+using DataLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyCms.Controllers
+{
+    public class AccountController : Controller
+    {
+        ILoginRepository _loginRepository;
+        MyCmsContext db = new MyCmsContext();
+        public AccountController()
+        {
+            _loginRepository = new LoginRepository(db);
+        }
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
